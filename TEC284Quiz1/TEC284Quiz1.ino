8dcf23fd4bfafbbd7af4cfff47b0a85703dac01c
epeterson02 <erikpeterson335@gmail.com>
2025-02-20 14:54:34 -0600

    Set pins and buttons
    Also created setColor Method

--- TEC284Quiz1/TEC284Quiz1.ino
+++ TEC284Quiz1/TEC284Quiz1.ino
@@ -1,9 +1,40 @@
-void setup() {
-  // put your setup code here, to run once:
+// Pins for LED
+const int redPin = 9;
+const int greenPin = 10;
+const int bluePin = 11;
 
+// Pins for buttons
+const int redButton = 2;
+const int greenButton = 3;
+const int blueButton = 4;
+
+// Stores current color state when button is pressed
+int redState = 0;
+int greenState = 0;
+int blueState = 0;
+
+// Function to update LED color
+void setColor(int red, int green, int blue) {
+    analogWrite(redPin, red);
+    analogWrite(greenPin, green);
+    analogWrite(bluePin, blue);
+}
+
+void setup() {
+    pinMode(redPin, OUTPUT);
+    pinMode(greenPin, OUTPUT);
+    pinMode(bluePin, OUTPUT);
+
+    pinMode(redButton, INPUT_PULLUP); // Pullup resistor to avoid noise
+    pinMode(greenButton, INPUT_PULLUP);
+    pinMode(blueButton, INPUT_PULLUP);
+
+    setColor(0, 0, 0);  // Start with LED off
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+    redState = !digitalRead(redButton);
+    greenState = !digitalRead(greenButton);
+    blueState = !digitalRead(blueButton);
+   
 }
